5f498df7f00db4c857ee160484a90076756911a4
rrtechmart <reza.cpbd@gmail.com>
2023-07-12 19:25:51 +0600

    I already set the basic route

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-5xl bg-yellow-500">This is home page </h1>
+        </div>
+    );
+};
+
+export default Home;
